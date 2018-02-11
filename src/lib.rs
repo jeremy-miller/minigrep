3268b334c5a66ff3bd01593c954c41da6c8c6f54
Jeremy Miller <jgmiller88@gmail.com>
2018-02-11 15:21:50 -0800

    move unit tests to doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! # Minigrep
+//!
 //! `minigrep` is a tool for searching a particular file for specific text.  It can perform
 //! both case-sensitive and case-insensitive searches.
 
@@ -22,13 +23,16 @@ impl Config {
     /// Creates a new [`Config`](struct.Config.html).
     ///
     /// # Arguments
+    ///
     /// * `args` - Command line arguments for this `minigrep` execution.
     ///
     /// # Return
+    ///
     /// `Result` of either a valid [`Config`](struct.Config.html), or an `Err` with a string
     /// literal error message.
     ///
     /// # Errors
+    ///
     /// * Returns a string literal `Err` if no [`query`](struct.Config.html#structfield.query)
     ///   argument is passed.
     /// * Returns a string literal `Err` if no [`filename`](struct.Config.html#structfield.filename)
@@ -61,13 +65,16 @@ impl Config {
 /// configuration parameters.  Prints matching lines to `stdout`.
 ///
 /// # Arguments
+///
 /// * `config` - [`Config`](struct.Config.html) containing the arguments to use for this search
 /// execution.
 ///
 /// # Return
+///
 /// `Result` of either `()` if successful, or an `Error` trait object.
 ///
 /// # Errors
+///
 /// * Returns errors related to opening [`filename`](struct.Config.html#structfield.filename)
 ///   (e.g. file doesn't exist, permissions issues, etc).
 /// * Returns an error if [`filename`](struct.Config.html#structfield.filename) does not contain
@@ -94,13 +101,28 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 /// Executes a case-sensitive text search of a string.
 ///
 /// # Arguments
+///
 /// * `query` - [`query`](struct.Config.html#structfield.query) to search for.
 /// * `contents` - The full text string to search for [`query`](fn.search.html#arguentsfield.query)
 ///                instances.
 ///
 /// # Return
+///
 /// `Vec` of lines from [`contents`](fn.search.html#arguentsfield.contents) which match the
 /// [`query`](fn.search.html#arguentsfield.query).
+///
+/// # Examples
+///
+/// ```
+/// let query = "duct";
+/// let contents = "\
+///Rust:
+///safe, fast, productive.
+///Pick three.
+///Duct tape.";
+///
+/// assert_eq!(vec!["safe, fast, productive."], minigrep::search(query, contents));
+/// ```
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
@@ -112,48 +134,34 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 /// `CASE_INSENSITIVE` environment variable is set when calling [`run`](fn.run.html).
 ///
 /// # Arguments
+///
 /// * `query` - [`query`](struct.Config.html#structfield.query) to search for.
 /// * `contents` - The full text string to search for
 ///                [`query`](fn.search_case_insensitive.html#arguentsfield.query) instances.
 ///
 /// # Return
+///
 /// `Vec` of lines from [`contents`](fn.search_case_insensitive.html#arguentsfield.contents) which
 /// match the [`query`](fn.search_case_insensitive.html#arguentsfield.query).
+///
+/// # Examples
+///
+/// ```
+/// let query = "rUsT";
+/// let contents = "\
+///Rust:
+///safe, fast, productive.
+///Pick three.
+///Trust me.";
+///
+/// assert_eq!(
+///     vec!["Rust:", "Trust me."],
+///     minigrep::search_case_insensitive(query, contents)
+/// );
+/// ```
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
         .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
         .collect()
-}
-
-#[cfg(test)]
-mod lib_tests {
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
-    }
 }
